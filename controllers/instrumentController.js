34d14c300a0564b37e6311ae36ef9197b4321d43
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-10-21 21:36:37 -0300

    correction

--- controllers/instrumentController.js
+++ controllers/instrumentController.js
@@ -1,4 +1,4 @@
-const { body, validationResult} = require("express-validator");
+const { body, validationResult } = require("express-validator");
 const Instrument = require("../models/instrument");
 const Artist = require("../models/artist");
 const Genre = require("../models/genre");
@@ -6,3 +6,34 @@ const Genre = require("../models/genre");
 const async = require("async");
 
 // Remember, in here we need to implement controllers for every url we create in in our routes files
+// Instrument refers artists and genres, so, you can delete inst, but NOT artist or genres, its deletion would cause
+// an empty data space for instrument
+
+exports.index = (req, res) => {
+  res.send("NOT IMPLEMENTED: BookInstance update GET");
+};
+
+exports.instrument_create_get = (req, res, next) => {
+  // get artist and genres for filling our instruments
+  // async.parallel(
+  //   {
+  //     artists(callback) {
+  //       Artist.find(callback);
+  //     },
+  //     genres(callback) {
+  //       Genre.find(callback);
+  //     },
+  //   },
+  //   (err, results) => {
+  //     if (err) {
+  //       return next(err);
+  //     }
+  //     res.render("instrument_form", {
+  //       title: "Create new Instrument",
+  //       artists: results.artists,
+  //       genres: results.genres,
+  //     });
+  //   }
+  // );
+  res.send("NOT IMPLEMENTED: BookInstance update GET");
+};
--- routes/inventory.js
+++ routes/inventory.js
@@ -15,20 +15,22 @@ router.get("/", instrument_controller.index);
 router.get("/instrument/create", instrument_controller.instrument_create_get);
 
 // POST request for creating a new instrument
-router.post("/instrument/create", instrument_controller.instrument_create_post);
+// router.post("/instrument/create", instrument_controller.instrument_create_post);
 
 // ------------ ARTIST ROUTES ------------ //
 
-// GET request for creating a new instrument
-router.get("/artist/create", artist_controller.artist_create_get);
+// // GET request for creating a new instrument
+// router.get("/artist/create", artist_controller.artist_create_get);
 
-// POST request for creating a new instrument
-router.get("/artist/create", artist_controller.artist_create_post);
+// // POST request for creating a new instrument
+// router.get("/artist/create", artist_controller.artist_create_post);
 
-// ------------ GENRE ROUTES ------------ //
+// // ------------ GENRE ROUTES ------------ //
 
-// GET request for creating a new instrument
-router.get("/genre/create", genre_controller.genre_create_get);
+// // GET request for creating a new instrument
+// router.get("/genre/create", genre_controller.genre_create_get);
 
-// POST request for creating a new instrument
-router.post("/genre/create", genre_controller.genre_create_post);
+// // POST request for creating a new instrument
+// router.post("/genre/create", genre_controller.genre_create_post);
+
+module.exports = router;
