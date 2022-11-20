92a4d672036fbfed7d4b81983bce8d5b0bbb41df
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-11-20 18:40:13 -0300

    modified .gitignore

--- client/src/Context/Context.jsx
+++ client/src/Context/Context.jsx
@@ -1,7 +1,5 @@
 import { createContext } from "react";
 
-export const megaObjeto = createContext({})
-export const dataClass = createContext('')
 export const artistObjeto = createContext({})
 export const genreObjeto = createContext({})
 export const instrumentObjeto = createContext({})
