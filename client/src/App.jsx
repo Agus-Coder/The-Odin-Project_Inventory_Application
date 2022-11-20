425e947d65ef9603fa24a038749c905d3de00d36
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-11-20 19:41:33 -0300

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import NavBar from "./NavBar/NavBar";
 import { BrowserRouter } from "react-router-dom";
 import InventoryContainer from "./InventoryContainer/InventoryContainer";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <InventoryContainer />
+      <Footer/>
     </BrowserRouter>
   );
 }
