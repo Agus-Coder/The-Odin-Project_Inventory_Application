95d30db018db658b3b2372b743a07ddbdcdda3de
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-12-03 12:13:46 -0300

    Login first coding

--- server/app.js
+++ server/app.js
@@ -67,4 +67,20 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) { 
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    });
+  })
+);
 // -------------Authentication Basics module------------- //
