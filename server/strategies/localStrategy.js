dd2a4400f92af600244801e31c5bd029638b0349
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-12-17 15:06:39 -0300

    verifyFunction added

--- server/strategies/localStrategy.js
+++ server/strategies/localStrategy.js
@@ -8,3 +8,23 @@ const customFields = {
   usernameField: "uname",
   passwordField: "pw",
 };
+
+const verifyFunction = (username, password, end) => {
+  User.findOne({ username: username })
+    .then((user) => {
+      if (!user) {
+        return end(null, false);
+      }
+
+      const isValid = validatePassFunction(password, user.hash, user.salt);
+
+      if (isValid) {
+        return end(null, user);
+      } else {
+        return end(null, false);
+      }
+    })
+    .catch((err) => {
+      end(err);
+    });
+};
