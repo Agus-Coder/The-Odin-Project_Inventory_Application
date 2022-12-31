cccd8a81e338c9135c654d98bd9f521b2cf985e7
Agus-Coder <agus_code@outlook.com.ar>
2022-12-31 16:16:39 -0300

    Added logic for already existing users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,21 +2,37 @@ const User = require("../models/user");
 const genPassword = require("../routes/Auth/passUtils").genPassword;
 
 exports.user_creation_post = (req, res, next) => {
-  const saltHash = genPassword(req.body.password);
-
-  const salt = saltHash.salt;
-  const hash = saltHash.hash;
-
-  const user = new User({
-    username: req.body.username,
-    hash: hash,
-    salt: salt,
-  });
-
-  user.save((err) => {
-    if (err) {
-      return next(err);
+  const userAlreadyExist = User.exists(
+    { username: req.body.username },
+    function (err, doc) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Result :", doc); // false
+      }
     }
-  });
-  next()
+  );
+
+  if (userAlreadyExist) {
+    const saltHash = genPassword(req.body.password);
+
+    const salt = saltHash.salt;
+    const hash = saltHash.hash;
+
+    const user = new User({
+      username: req.body.username,
+      hash: hash,
+      salt: salt,
+    });
+
+    user.save((err) => {
+      if (err) {
+        return next(err);
+      }
+    });
+    next();
+  } else {
+    console.log("user already exists!");
+    next();
+  }
 };
